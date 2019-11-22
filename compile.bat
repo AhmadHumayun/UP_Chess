61b33752d3004e6947bcac4a75cda649d1fac000
Ahmad <ahmadhumayun3.5@gmail.com>
2019-11-22 16:11:38 +0500

    Added batch file for compiling with g++

---
+++ compile.bat
@@ -0,0 +1,3 @@
+g++ -c -std=c++11 Bishop.cpp GameManager.cpp King.cpp Knight.cpp Pawn.cpp Piece.cpp Queen.cpp Renderer.cpp Rook.cpp Source.cpp Utils.cpp
+g++ Bishop.o GameManager.o King.o Knight.o Pawn.o Piece.o Queen.o Renderer.o Rook.o Source.o Utils.o -lgdi32
+a.exe
